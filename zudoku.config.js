a9180d406ed8add1a7d8a2278e6c05f20ec0e898
Caleb Smiler <215513221+keffjoons@users.noreply.github.com>
2025-10-26 22:54:03 +0000

    Add zudoku.config.js to enable developer portal

---
+++ zudoku.config.js
@@ -0,0 +1,37 @@
+export default {
+  page: {
+    title: "LooksyAI API Documentation",
+  },
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Getting Started",
+        items: [
+          "getting-started/authentication",
+          "getting-started/quick-start",
+        ],
+      },
+      {
+        type: "doc",
+        id: "api-reference",
+        label: "API Reference",
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs" }],
+  apis: [
+    {
+      type: "file",
+      input: "./config/routes.oas.json",
+      navigationId: "api",
+    },
+  ],
+  docs: {
+    files: "/docs/pages/**/*.{md,mdx}",
+  },
+};
